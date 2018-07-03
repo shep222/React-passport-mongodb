3d9f62000adde408ec86c1408fd0879527f1d628
John E Shepler Jr <jshepler@suitedconnector.com>
2018-07-03 14:11:39 -0600

    create token

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mondoURI: 'mongodb://john:shep6331@ds231658.mlab.com:31658/devconnector222'
+  mondoURI: 'mongodb://john:shep6331@ds231658.mlab.com:31658/devconnector222',
+  secretorKey: 'secret'
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const brcypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys')
+
+
+// Load User Model
+const User = require('./../models/User');
 
 
 // @route GET api/users/test
@@ -8,6 +16,102 @@ const router = express.Router();
 router.get('/test', (req, res) => res.json({
   msg: "Users Works"
 }));
+
+// @route GET api/users/register
+// @desc Register User
+// @access Public
+router.post('/register', (req, res) => {
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
+    if (user) {
+      console.log('FAIL')
+      return res.status(400).json({
+        email: 'Email already exists;'
+      })
+    } else {
+      console.log('PASS')
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', // size
+        r: 'pg', // rating
+        d: 'mm' // default image
+      })
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      })
+
+      brcypt.genSalt(10, (err, salt) => {
+        brcypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser.save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err))
+        })
+      })
+    }
+  })
+})
+
+// @route GET api/users/login
+// @desc Login User / Return JWT Token
+// @access Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Find user email
+  User.findOne({
+      email
+    })
+    .then(user => {
+      //Check for user
+      if (!user) {
+        return res.status(404).json({
+          email: 'User not found'
+        })
+      }
+
+      //Check Password
+      brcypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            //User Matched
+            const payload = {
+              id: user.id,
+              name: user.name,
+              avatar: user.avatar
+            } //Create JWT Payload
+
+            //Sign Webtoken
+            jwt.sign(
+              payload,
+              keys.secretorKey, {
+                expiresIn: 3600
+              },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                });
+              })
+          } else {
+            return res.status(400).json({
+              password: 'Password Incorrect'
+            })
+          }
+        })
+    })
+
+
+})
+
+
+
 
 
 module.exports = router;
